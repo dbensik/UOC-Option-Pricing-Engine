051366ec5083f806a3916edd31cd70935172fa07
Danny <dbensik@users.noreply.github.com>
2025-06-12 22:10:17 -0400

    Update GslTridiagonalSolver.hpp

--- src/pricing/GslTridiagonalSolver.hpp
+++ src/pricing/GslTridiagonalSolver.hpp
@@ -1,16 +1,18 @@
+// GslTridiagonalSolver.hpp
 #ifndef GSL_TRIDIAGONAL_SOLVER_HPP
 #define GSL_TRIDIAGONAL_SOLVER_HPP
 
-#include <vector>
+#include "TridiagonalSolverBase.hpp"
+#include <gsl/gsl_linalg.h>
 
-class GslTridiagonalSolver {
+class GslTridiagonalSolver : public TridiagonalSolverBase {
 public:
     std::vector<double> solve(
         const std::vector<double>& a,
         const std::vector<double>& b,
         const std::vector<double>& c,
-        const std::vector<double>& d
-    );
+        const std::vector<double>& rhs
+    ) const override;
 };
 
-#endif
+#endif // GSL_TRIDIAGONAL_SOLVER_HPP

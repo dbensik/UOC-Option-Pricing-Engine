407d3b9c4df893f45cec12b2afdfe886b24919f8
Danny <dbensik@users.noreply.github.com>
2025-06-12 22:10:00 -0400

    Update CustomTridiagonalSolver.cpp

--- src/pricing/CustomTridiagonalSolver.cpp
+++ src/pricing/CustomTridiagonalSolver.cpp
@@ -1,32 +1,24 @@
-
-#include "TridiagonalSolverBase.hpp"
-#include <stdexcept>
+// CustomTridiagonalSolver.cpp
+#include "CustomTridiagonalSolver.hpp"
+#include <vector>
 
 std::vector<double> CustomTridiagonalSolver::solve(
     const std::vector<double>& a,
     const std::vector<double>& b,
     const std::vector<double>& c,
-    const std::vector<double>& d
-) {
-    int n = b.size();
-    std::vector<double> c_prime(n, 0.0);
-    std::vector<double> d_prime(n, 0.0);
-    std::vector<double> x(n, 0.0);
-
-    if (a.size() != n - 1 || c.size() != n - 1 || d.size() != n) {
-        throw std::invalid_argument("Invalid input sizes for tridiagonal solver.");
-    }
+    const std::vector<double>& rhs
+) const {
+    int n = rhs.size();
+    std::vector<double> x(n), c_prime(n), d_prime(n);
 
     c_prime[0] = c[0] / b[0];
-    d_prime[0] = d[0] / b[0];
+    d_prime[0] = rhs[0] / b[0];
 
-    for (int i = 1; i < n - 1; ++i) {
+    for (int i = 1; i < n; ++i) {
         double m = b[i] - a[i - 1] * c_prime[i - 1];
         c_prime[i] = c[i] / m;
-        d_prime[i] = (d[i] - a[i - 1] * d_prime[i - 1]) / m;
+        d_prime[i] = (rhs[i] - a[i - 1] * d_prime[i - 1]) / m;
     }
-
-    d_prime[n - 1] = (d[n - 1] - a[n - 2] * d_prime[n - 2]) / (b[n - 1] - a[n - 2] * c_prime[n - 2]);
 
     x[n - 1] = d_prime[n - 1];
     for (int i = n - 2; i >= 0; --i) {
